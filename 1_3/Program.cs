bd4d93175613af34b63b185af5717d19408c5d64
tanas <tanas@FOXIK-PC>
2023-10-10 18:50:56 +0300

    Добавьте файлы проекта.

---
+++ 1_3/Program.cs
@@ -0,0 +1,6 @@
+﻿double a, b; //переменные
+Console.WriteLine("Введите первое число: ");
+a = Convert.ToDouble(Console.ReadLine()); // вводимое значение 1
+Console.WriteLine("Введите второе число: ");
+b = Convert.ToDouble(Console.ReadLine()); // вводимое значение 2
+Console.WriteLine($"Сумма: {a + b}\nВычитание: {a - b}\nПроизведение: {a * b}\nДеление: {a / b} "); // ответ
